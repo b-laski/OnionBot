c72c4a92af234bf0e50ed073d4de71a97627afde
Bartłomiej Łaski <tajfun695@gmail.com>
2017-02-12 23:57:38 +0100

    mainWindow first changes

--- OnionBot/MainWindow.xaml.cs
+++ OnionBot/MainWindow.xaml.cs
@@ -23,6 +23,41 @@ namespace OnionBot
         public MainWindow()
         {
             InitializeComponent();
+            btnMaxiWindow.AddHandler(FrameworkElement.MouseDownEvent, new MouseButtonEventHandler(btnMaxiWindow_Click), true);
+            btnMinWindow.AddHandler(FrameworkElement.MouseDownEvent, new MouseButtonEventHandler(btnMinWindow_Click), true);
         }
+
+        #region Buttons
+        private void btnCloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMaxiWindow_Click(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void btnMinWindow_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+
+        #endregion
     }
 }
