99b9338151a4e43b2bae83a6fec16ab1b2cae3ec
Bartlomiej Laski <tajfun695@gmail.com>
2017-02-23 14:09:11 +0100

    better weblogin

--- OnionBot/UserControls/Usercontrol_Settings/Account/AccountForm.xaml.cs
+++ OnionBot/UserControls/Usercontrol_Settings/Account/AccountForm.xaml.cs
@@ -29,28 +29,8 @@ namespace OnionBot.UserControls.Usercontrol_Settings.Account
 
         private void btnTwitchConnect(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start(@"https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&amp;client_id=a4a9z3lf5b00it271smlkaxdajvf0un&amp;redirect_uri=http://localhost:8090&amp;scope=user_read+user_blocks_edit+user_blocks_read+user_follows_edit+channel_read+channel_editor+channel_commercial+channel_stream+channel_subscriptions+user_subscriptions+channel_check_subscription+chat_login");
-            var linkListener = new HttpListener();
-            linkListener.Prefixes.Add("http://localhost:8090/");
-            linkListener.Prefixes.Add("http://127.0.0.1:8090/");
-            linkListener.Start();
-
-            HttpListenerContext context = linkListener.GetContext();
-            HttpListenerRequest request = context.Request;
-            HttpListenerResponse response = context.Response;
-
-            string responseMessage = "<html><head><title>Connecting with you Twitch account</title><script type=\"text / javascript\"> \nvar http = new XMLHttpRequest(); \nvar url = \" / auth - token\"; \nhttp.open(\"POST\", url, true); \nhttp.setRequestHeader(\"Content - Type\", \"application / json / \"); \nhttp.send(JSON.stringify({authToken: hash})); </script></head><body></body></html>";
-            byte[] buffer = Encoding.UTF8.GetBytes(responseMessage);
-
-            var cos = response.OutputStream.;
-            MessageBox.Show(cos);
-
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            output.Close();
-
-            linkListener.Stop();
+            TwitchLogin loginWindow = new TwitchLogin();
+            loginWindow.Show();
         }
     }
 }
--- OnionBot/UserControls/Usercontrol_Settings/Account/TwitchLogin.xaml.cs
+++ OnionBot/UserControls/Usercontrol_Settings/Account/TwitchLogin.xaml.cs
@@ -22,6 +22,7 @@ namespace OnionBot.UserControls.Usercontrol_Settings.Account
         public TwitchLogin()
         {
             InitializeComponent();
+            webClient.Address = "https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&amp;client_id=a4a9z3lf5b00it271smlkaxdajvf0un&amp;redirect_uri=http://localhost:8090&amp;scope=user_read+user_blocks_edit+user_blocks_read+user_follows_edit+channel_read+channel_editor+channel_commercial+channel_stream+channel_subscriptions+user_subscriptions+channel_check_subscription+chat_login";
         }
 
         private void btnCloseApp_Click(object sender, RoutedEventArgs e)
